4b1bea06498e8c5eb794150a1cddc3ad39ffff69
Shane Neuville <shneuvil@microsoft.com>
2018-12-02 13:37:10 -0700

    removing maps fro samples

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/PerformanceGallery/Scenarios/MapScenarios.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/PerformanceGallery/Scenarios/MapScenarios.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Linq;
-using Xamarin.Forms.Internals;
-using Xamarin.Forms.Maps;
+﻿//using System;
+//using System.Linq;
+//using Xamarin.Forms.Internals;
+//using Xamarin.Forms.Maps;
 
-namespace Xamarin.Forms.Controls.GalleryPages.PerformanceGallery.Scenarios
-{
-	[Preserve(AllMembers = true)]
-	internal class MapScenario1 : PerformanceScenario
-	{
-		public MapScenario1()
-		: base("[Map] Empty")
-		{
-			View = new Map();
-		}
-	}
-}
+//namespace Xamarin.Forms.Controls.GalleryPages.PerformanceGallery.Scenarios
+//{
+//	[Preserve(AllMembers = true)]
+//	internal class MapScenario1 : PerformanceScenario
+//	{
+//		public MapScenario1()
+//		: base("[Map] Empty")
+//		{
+//			View = new Map();
+//		}
+//	}
+//}
--- Xamarin.Forms.Controls/GalleryPages/CellsGalleries/UnEvenViewCellGallery.cs
+++ Xamarin.Forms.Controls/GalleryPages/CellsGalleries/UnEvenViewCellGallery.cs
@@ -10,25 +10,25 @@ namespace Xamarin.Forms.Controls
 	{
 		public UnevenViewCellGallery ()
 		{
-			Title = "UnevenViewCell Gallery - Legacy";
+			//Title = "UnevenViewCell Gallery - Legacy";
 
-			var map = MapGallery.MakeMap ();
-			map.HasScrollEnabled = false;
+			//var map = MapGallery.MakeMap ();
+			//map.HasScrollEnabled = false;
 
-			Content = new TableView {
-				RowHeight = 150,
-				HasUnevenRows = true,
-				Root = new TableRoot {
-					new TableSection ("Testing") {
-						new ViewCell {View = map, Height = 250},
-						new ViewCell {View = new ProductCellView ("1 day")},
-						new ViewCell {View = new ProductCellView ("2 days")},
-						new ViewCell {View = new ProductCellView ("3 days")},
-						new ViewCell {View = new ProductCellView ("4 days")},
-						new ViewCell {View = new ProductCellView ("5 days")}
-					}
-				}
-			};
+			//Content = new TableView {
+			//	RowHeight = 150,
+			//	HasUnevenRows = true,
+			//	Root = new TableRoot {
+			//		new TableSection ("Testing") {
+			//			new ViewCell {View = map, Height = 250},
+			//			new ViewCell {View = new ProductCellView ("1 day")},
+			//			new ViewCell {View = new ProductCellView ("2 days")},
+			//			new ViewCell {View = new ProductCellView ("3 days")},
+			//			new ViewCell {View = new ProductCellView ("4 days")},
+			//			new ViewCell {View = new ProductCellView ("5 days")}
+			//		}
+			//	}
+			//};
 		}
 	}
 }
